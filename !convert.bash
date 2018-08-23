41aa305743acf1c9112213b48f6782eae2486ce2
dotMitsu <arutyun-akopov@yandex.ru>
2018-08-24 00:41:59 +0300

    Version 1.0

---
+++ !convert.bash
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#Параметры для FFMPEG
+#FFMPEG Parameters
+PARAMETERS='-acodec aac -strict -2 -b:a 128k -vcodec libx264 -b:v 10000k -r 25 -ac 2'
+
+ls | while read FILENAME;
+do
+
+#Удаляем из имени файла всё после последней точки, т.е. удаляем расширение файла.
+#Чтобы у сконвертированных файлов не было дублирования расширения в названии.
+#Delete file extension from filename.
+FILENAME_WITHOUT_EXT=$(echo "$FILENAME" | sed 's/\.[^.]*$//');
+
+#Директория в которую будут сохраняться переконвертированные файлы
+#The directory where converted files will be saved
+OUTPUT="./converted/conv.$FILENAME_WITHOUT_EXT.mp4"
+
+#Расширения файлов, которые будет искать скрипт в корневой папке, а затем конвертировать
+#Extensions of files that will search the script in the root folder, and then convert
+if [[ $FILENAME = *.mpg || $FILENAME = *.mp4 || $FILENAME = *.mpeg || $FILENAME = *.mxf || $FILENAME = *.mts || $FILENAME = *.mkv ]]
+    then
+        ffmpeg -hide_banner -i "$FILENAME" $PARAMETERS "$OUTPUT" < /dev/null
+fi
+
+done
+
+echo 'Нажмите любую клавишу'
+read -n 1
